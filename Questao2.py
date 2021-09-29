89ce366b4c4aa5f956a724dc04601acdc3c7f21b
joaopedropilastri <joaopedropilastri@gmail.com>
2021-09-29 16:21:38 -0300

    em desenvolvimento

--- Questao2.py
+++ Questao2.py
@@ -14,6 +14,21 @@ def ler_fasta(arquivo):
 # testando...
 #codon inicio ATG, codon parada TAA, TAG, TGA
 lista = ler_fasta("gene_S_Whuhan.fasta")
+listaaux=[]
 cont=0
+contaux=0
 for i in lista:     
-    print(i)  
+    while (contaux<len(i)):
+        cont=cont+1
+        contaux=contaux+1
+        if cont==3:
+            listtemp=[]
+            listtemp.append(i[contaux-3])
+            listtemp.extend(i[contaux-2])
+            listtemp.extend(i[contaux-1])
+            listaaux.append(listtemp)
+            listtemp=[]
+            cont=0
+        
+print(listaaux)
+
